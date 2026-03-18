15051df174048bc1ee7f1f76caf75315a6c680cd
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-03-18 16:52:49 +0100

    Setup del form con useState e functions, submit e  handleData

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,36 +2,48 @@ import { useState } from "react"
 
 export default function MainContent() {
 
-    const [author, setAuthor] = useState('')
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    // const [publics, setPublics] = useState('false')
+    const [formData, setFormData] = useState({
+        author: "",
+        title: "",
+        body: "",
+        switchCheckDefault: false
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+    function handleDataForm(e) {
+        const dataValue = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        setFormData({ ...formData, [e.target.name]: dataValue })
+        console.log(formData)
+    }
 
     return (
 
         <>
             <div className="container">
-                <form className="row" >
-                    <div className="col-12 mb-3">
-                        <label for="exampleFormControlInput1" className="form-label text-danger fs-4"><strong>Your favorite Author:</strong></label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="The name of your favorite Author" value={author} onChange={(e) => { setAuthor(e.target.value) }} />
+                <form className="row g-3" onSubmit={handleSubmit} >
+                    <div className="col-12">
+                        <label htmlFor="author" className="form-label text-danger fs-4"><strong>Your favorite Author:</strong></label>
+                        <input type="text" className="form-control" id="author" name="author" placeholder="The name of your favorite Author" value={formData.author} onChange={handleDataForm} />
                     </div>
-                    <div className="col-12 mb-3">
-                        <label for="exampleFormControlInput1" className="form-label text-danger fs-4"><strong>Bookname:</strong></label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="The name of the book" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+                    <div className="col-12">
+                        <label htmlFor="title" className="form-label text-danger fs-4"><strong>Bookname:</strong></label>
+                        <input type="text" className="form-control" id="title" name="title" placeholder="The name of the book" value={formData.title} onChange={handleDataForm} />
                     </div>
-                    <div className="col-12 mb-3">
-                        <label for="exampleFormControlTextarea1" className="form-label text-danger fs-4"><strong>The plot: </strong></label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={body} onChange={(e) => { setBody(e.target.value) }}></textarea>
+                    <div className="col-12">
+                        <label htmlFor="body" className="form-label text-danger fs-4"><strong>The plot: </strong></label>
+                        <textarea className="form-control" id="body" rows="3" name="body" value={formData.body} onChange={handleDataForm}></textarea>
                     </div>
                     <div className="col-12">
                         <div className="form-check form-switch">
-                            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" />
-                            <label className="form-check-label text-danger" for="switchCheckDefault"><strong>Public your vlog</strong></label>
+                            <input className="form-check-input" type="checkbox" role="switch" id="switchCheckDefault" name="switchCheckDefault" onChange={handleDataForm} checked={formData.switchCheckDefault} />
+                            <label className="form-check-label text-danger" htmlFor="switchCheckDefault"><strong>Public your vlog</strong></label>
                         </div>
                     </div>
                     <div className="col-12">
-                        <button type="button" class="btn btn-danger my-3"><strong>Submit</strong></button>
+                        <button type="button" className="btn btn-danger my-3"><strong>Submit</strong></button>
                     </div>
                 </form>
             </div>
